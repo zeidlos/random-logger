fd1d826b7930ffa3c41c303516111adea1639152
Julius Zeidler <juliuszeidler@delt-lmvt9y1jw2.fritz.box>
2024-02-28 17:18:28 +0100

    refactor logger

--- logger.js
+++ logger.js
@@ -1,8 +1,9 @@
-const interval = setInterval(function () {
+function logger() {
   console.log("random-logger");
   console.log(
     '{"data": {"context": {},"PORT": 4003,"PATH": "/middleware","SENTRY_DSN": "https://abc@host.name/50","LOG_FORMAT": "logstash","LOG_LEVEL": "info","EXT_SYSTEM_URL": "","PROJECT_NAME": "name","K8S_NS": "foobar","SENTRY_CURRENT_ENV": "production"},"tags": {"application": "random-logger","release": "0.0.1","project_name": "random-logger","k8s_ns": "random-logger","type": "configuration"},"level": "info","message": "none"})',
   );
-}, 5000);
+  setTimeout(logger, 5000);
+}
 
-clearInterval(interval);
+logger();
